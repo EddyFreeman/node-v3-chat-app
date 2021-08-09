82dee0726a03933a22e9ef9c3e2d5f5091600816
EddyFreeman <win1for@yahoo.com>
2021-08-09 20:07:33 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -81,7 +81,7 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         const user = removeUser(socket.id)
         if (user) {
-            io.to(user.room).emit('message', generateMessage(user.username, 'has left'))
+            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left`))
 
             io.to(user.room).emit('roomData', {
                 room: user.room,
